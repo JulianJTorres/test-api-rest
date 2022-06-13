bad6dbbed064929f937429b29628300f11f24f07
juliantorres <julipage@gmail.com>
2022-06-13 11:25:09 +0200

    Added is parameter

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -6,7 +6,12 @@ const data = require('./cards.json');
 
 //Cards
 router.get('/', (req, res) => {
-  res.json(data);
+  if (req.query.id){
+    const elem = data.filter(c => req.query.id == c.id);
+    res.json(elem);
+  }
+  else
+    res.json(data);
 });
 
 module.exports = router;
